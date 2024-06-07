ef06e8e3a603e277d4aedc92e8ce1a2ac8f8cd2a
Nemuszz <nsavic3011@gmail.com>
2024-06-07 14:51:30 +0200

    Finished Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,116 @@
+import  {Link} from 'react-router-dom';
+import NatureImage from '../../assets/naturepic.jpg'
+import FooterLogo from '../../assets/logotravel.png'
+import {FaFacebook, FaInstagram, FaLinkedin, FaLocationArrow, FaMobileAlt} from "react-icons/fa";
+const FooterLinks= [
+    {
+        title: 'Home',
+        link:'/'
+    },
+    {
+        title: 'About',
+        link:'/about'
+    },
+    {
+        title: 'Best Places',
+        link:'/best-places'
+    },
+    {
+        title: 'Blogs',
+        link:'/blogs'
+    },
+]
 const Footer = () =>{
-    return(
-        <div>Footer</div>
-    )
-}
+
+
+    return (
+        <>
+            <div className="dark:bg-gray-950 py-10 relative overflow-hidden">
+                <img src={NatureImage} alt='Nature image' className='absolute top-0 left-0 w-full rounded h-full' />
+                <div className="container">
+                    <div className="grid md:grid-cols-3 py-5 bg-white/80 backdrop-blur-sm rounded-t-xl">
+                        <div className="py-8 px-4">
+                            <h1 className="flex items-center gap-3 text-xl sm:text-3xl font-bold text-justify sm:text-left">
+                                <img src={FooterLogo} alt="" className="max-h-[60px]" />
+                                {/* TravelloGo */}
+                            </h1>
+                            <p className="text-sm">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                                facere ab hic accusamus omnis dolor voluptatibus illo, tempore
+                                eum tenetur.
+                            </p>
+                            <br />
+                            <div className="flex items-center gap-3 ">
+                                <FaLocationArrow />
+                                <p>Noida, Uttar Pradesh</p>
+                            </div>
+                            <div className="flex items-center gap-3 mt-3">
+                                <FaMobileAlt />
+                                <p>+91 123456789</p>
+                            </div>
+                            {/* social handles */}
+                            <div>
+                                <div className="flex items-center gap-3 mt-6">
+                                    <a href="#">
+                                        <FaInstagram className="text-3xl" />
+                                    </a>
+                                    <a href="#">
+                                        <FaFacebook className="text-3xl" />
+                                    </a>
+                                    <a href="#">
+                                        <FaLinkedin className="text-3xl" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+                            <div>
+                                <div className="py-8 px-4">
+                                    <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
+                                        Important Links
+                                    </h1>
+                                    <ul className="flex flex-col gap-3">
+                                        {FooterLinks.map((link) => (
+                                            <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                                                <Link
+                                                    to={link.link}
+                                                    onClick={() => window.scrollTo(0, 0)}
+                                                >
+                                                    <span>&#11162;</span>
+                                                    <span>{link.title}</span>
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="py-8 px-4">
+                                    <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
+                                        Important Links
+                                    </h1>
+                                    <ul className="flex flex-col gap-3">
+                                        {FooterLinks.map((link) => (
+                                            <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200">
+                                                <Link
+                                                    to={link.link}
+                                                    onClick={() => window.scrollTo(0, 0)}
+                                                >
+                                                    <span>&#11162;</span>
+                                                    <span>{link.title}</span>
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
 export default Footer
